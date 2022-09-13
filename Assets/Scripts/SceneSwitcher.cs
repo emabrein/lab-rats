8c014a96b19e0cf0529ec076a28a75cc3ccef331
emabrein <emabrein@gmail.com>
2022-09-13 13:40:29 -0300

    lo que tenia hecho

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void exitGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void showHistoria()
+    {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void hideHistoria()
+    {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
