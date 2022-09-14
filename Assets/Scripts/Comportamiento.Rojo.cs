289d0af2b2c56c78fcf55b6182bc4060fe939638
emabrein <110540116+emabrein@users.noreply.github.com>
2022-09-14 12:58:36 -0300

    x

--- Assets/Scripts/Comportamiento.Rojo.cs
+++ Assets/Scripts/Comportamiento.Rojo.cs
@@ -13,6 +13,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+	{
+		// Si se ha pulsado, modificar velocidad hacia arriba con una fuerza de 5
+		myRigidbody.velocity  =  Vector2.up  *  5.0f;
     }
+    if (Input.GetKeyDown(KeyCode.W))
+	{
+		// Si se ha pulsado, modificar velocidad hacia arriba con una fuerza de 5
+		myRigidbody.velocity  =  Vector2.up  *  5.0f;
 }
