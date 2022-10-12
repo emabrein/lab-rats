def0b7593e4d297f2daeabe5ec9fed5b11b33c85
tomi.moha <47349429@est.ort.edu.ar>
2022-10-12 12:11:44 -0300

    personaje

---
+++ Assets/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public Text cheeseCounter;
+
+    private int cheese = 0;
+
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Collectible")){
+            Collect(other.GetComponent<Collectible>());
+        }
+    }
+
+    private void Collect(Collectible collectible){
+        if(collectible.Collect()){
+            if(collectible is CheeseCollectible){
+                cheese++;
+            } 
+            UpdateGUI();
+        } 
+    }
+
+    private void UpdateGUI(){
+       cheeseCounter.text = cheese.ToString();
+    }
+}
